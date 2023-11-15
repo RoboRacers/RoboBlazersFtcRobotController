bf610d632e409db905cd15d99830d3be35ef371b
ishaan-ahuja <ishaan.ahuja.102@gmail.com>
2023-11-14 20:35:03 -0800

    drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/drive/StandardTrackingWheelLocalizer.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.modules.drive;
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.localization.ThreeTrackingWheelLocalizer;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.modules.util.Encoder;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Config
+public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer {
+
+    //REV THROUGH BORE encoder
+    public static double TICKS_PER_REV = 8192;
+
+    //https://www.studica.com/studica-robotics/38mm-omni-wheel
+    public static double WHEEL_RADIUS = 0.748031;
+    public static double GEAR_RATIO = 1;
+    public static double LATERAL_DISTANCE = 9.25;
+    public static double FORWARD_OFFSET = -7.5;
+    public static double X_MULTIPLIER = 1.0256410256;
+    public static double Y_MULTIPLIER = 1.0322580645;
+    private Encoder leftEncoder, rightEncoder, frontEncoder;
+    public StandardTrackingWheelLocalizer(HardwareMap hardwareMap) {
+        super(Arrays.asList(
+                new Pose2d(2, LATERAL_DISTANCE / 2, 0), // left
+                new Pose2d(2.5 -LATERAL_DISTANCE / 2, 0), // right
+                new Pose2d(FORWARD_OFFSET, -1, Math.toRadians(90)) // front
+        ));
+
+        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "Fl")); // Port Number 0
+        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "Bl")); // Port Number 2
+        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "Fr")); // Port Number 1
+
+        leftEncoder.setDirection(Encoder.Direction.REVERSE);
+        //TBD to check
+
+    }
+
+    public static double encoderTicksToInches(double ticks) {
+        return WHEEL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks / TICKS_PER_REV;
+    }
+
+    @NonNull
+    @Override
+    public List<Double> getWheelPositions() {
+        return Arrays.asList(
+                encoderTicksToInches(leftEncoder.getCurrentPosition()) * X_MULTIPLIER,
+                encoderTicksToInches(rightEncoder.getCurrentPosition()) * X_MULTIPLIER,
+                encoderTicksToInches(frontEncoder.getCurrentPosition()) * Y_MULTIPLIER
+        );
+    }
+
+    @NonNull
+    @Override
+    public List<Double> getWheelVelocities() {
+
+        return Arrays.asList(
+                encoderTicksToInches(leftEncoder.getCorrectedVelocity()) * X_MULTIPLIER,
+                encoderTicksToInches(rightEncoder.getCorrectedVelocity()) * X_MULTIPLIER,
+                encoderTicksToInches(frontEncoder.getCorrectedVelocity()) * Y_MULTIPLIER
+        );
+    }
+}
