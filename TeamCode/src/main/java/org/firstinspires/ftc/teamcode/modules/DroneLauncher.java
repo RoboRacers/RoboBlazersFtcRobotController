9fecd5aa8c382fd18ce52e64aff9b2e1b84e0dce
Mithilessh2010 <Mithib2010@hotmail.com>
2023-11-14 19:56:19 -0800

    Drone Launcher Fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/DroneLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/DroneLauncher.java
@@ -1,34 +1,36 @@
 package org.firstinspires.ftc.teamcode.modules;
 
-import static org.firstinspires.ftc.robotcontroller.external.samples.ConceptRampMotorSpeed.INCREMENT;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-public class DroneLauncher extends LinearOpMode {
+public class DroneLauncher{
     Servo tempServo;
     Servo droneServo;
 
     ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+    double LAUNCH_POSITION = 1.0;
+    double START_POSITION = 0.3;
+    double INCREMENT_POSITION = 0.05;
 
 
 
     public DroneLauncher(HardwareMap hardwareMap) {
         droneServo = hardwareMap.get(Servo.class, "droneServo");
+        droneServo.setPosition(START_POSITION);
     }
 
     public void launchDrone() {
+        timer.reset();
         double target = timer.time() + 5;
-        while(opModeIsActive()){
+        double current_position = START_POSITION;
+        while(current_position <= LAUNCH_POSITION){
             if(timer.time()>target){
-                target += 5;
-                double position = INCREMENT;
-                droneServo.setPosition(position);
+                current_position = current_position + INCREMENT_POSITION;
+                target = timer.time() + 5;
+                droneServo.setPosition(current_position);
             }
         }
     }
-
-    @Override
-    public void runOpMode(){}
 }
